cf21a2be2ab16bc40eebb5896f64fb9db76d6a56
luc16 <luc.joffily.ribas@gmail.com>
2021-09-19 11:34:36 -0300

    testando o git

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ RESOLUTION = ""
 FLAGS = f"-pm {RESOLUTION}"
 SCENE = "Test"
 
-
 class Test(Scene):
     def construct(self):
-        text = Text("Eai galera", color=TEAL)
-        self.play(FadeIn(text))
+        text = Text("teste 1 - trabalho de algebra linear", color=TEAL)
+        self.play(FadeOut(text))
         self.wait()
-
 
 if __name__ == '__main__':
     script_name = f"{Path(__file__).resolve()}"
     os.system(f"manim {script_name} {SCENE} {FLAGS}")
+    # print("hello world")
